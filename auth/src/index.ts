10a1cc1cb9cce87bdb7d10f9b26f25580965e2cc
kursat <k.dogan@student.fontys.nl>
2022-04-13 15:12:53 +0200

    Update test index

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import {app} from "./app";
 
 const start = async () => {
-    console.log('Starting up.!f...s.')
+    console.log('Starting up.!f.d..s.')
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
     }
